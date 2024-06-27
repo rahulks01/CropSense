1d8d92428c0bd877b45af81590efe7a25986fc2e
Rahul Kumar Singh <ks.rahul0111@gmail.com>
2024-06-27 12:35:41 +0530

    Set up basic webpages for authentication

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model) :
+    created_at = models.DateTimeField(auto_now_add=True)
+    crop_name = models.CharField(max_length=50)
+    season = models.CharField(max_length=50)
+    harvest_time = models.CharField(max_length=50)
+    quantity = models.IntegerField
+    price = models.IntegerField
+
+    def __str__(self) :
+        return(f"{self.crop_name}")
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import SignUpForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')  
+        else:
+            messages.error(request, 'Invalid username or password')
+    
+    return render(request, 'login.html')
+
+# @login_required(login_url='/login/') 
+def home(request):
+    return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request) 
+    return redirect('login')  
+
+def register_user(request) :
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            messages.success(request, "You Have Successfully Registered")
+            return redirect('home')
+    else :
+        form = SignUpForm()
+        return render(request, 'register.html', {'form' : form})
+
+    return render(request, 'register.html', {'form' : form})
